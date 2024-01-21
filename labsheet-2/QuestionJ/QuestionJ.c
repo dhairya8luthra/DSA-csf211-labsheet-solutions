839aa51733decd250bf25b833af61f8ec7381d00
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-01-21 19:30:29 +0530

    1

--- labsheet-2/QuestionJ/QuestionJ.c
+++ labsheet-2/QuestionJ/QuestionJ.c
@@ -6,4 +6,14 @@ int main()
     int dim;
     scanf("%d", &dim);
     //
+    int matrix[dim][dim];
+    int visited[dim][dim];
+    int number = 1;
+    int spiral = 0;
+    int i = 0;
+    int j = 0;
+
+    while (spiral != dim * dim)
+    {
+    }
 }
