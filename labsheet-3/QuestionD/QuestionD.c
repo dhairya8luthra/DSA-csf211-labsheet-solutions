9781fa67078f84df2ab44083974824a66f0d90ce
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-01-27 18:52:37 +0530

    solved QuestionD Jiraiya's Legacy lb-3

---
+++ labsheet-3/QuestionD/QuestionD.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int isVowel(char suspect)
+{
+    return (suspect == 'a' || suspect == 'e' || suspect == 'i' || suspect == 'o' || suspect == 'u');
+}
+
+int main()
+{
+    // taking input
+    int n;
+    scanf("%d", &n);
+    char sentence[n];
+    scanf(" %[^\n]s", sentence);
+
+    // initialization and approach
+    // the approach is going to be running two pointers, one from start and one from end
+    // when both pointers find vowels, exchange them
+
+    int start = 0;
+    int end = n - 1;
+    int found_left = 0;
+    int found_right = 0;
+
+    while (start < end)
+    {
+        if (!found_left && isVowel(sentence[start]))
+        {
+            found_left = 1;
+        }
+        else if (!found_left)
+        {
+            start++;
+        }
+
+        if (!found_right && isVowel(sentence[end]))
+        {
+            found_right = 1;
+        }
+        else if (!found_right)
+        {
+            end--;
+        }
+
+        if (found_left && found_right)
+        {
+            // swap
+            char temp = sentence[start];
+            sentence[start] = sentence[end];
+            sentence[end] = temp;
+
+            found_left = 0;
+            found_right = 0;
+            start++;
+            end--;
+        }
+    }
+
+    printf("%s", sentence);
+    return 0;
+}
