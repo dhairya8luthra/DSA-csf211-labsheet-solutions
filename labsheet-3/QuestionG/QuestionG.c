f0486e0b332beb2077c7ec39c8114839e445196f
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-01-28 04:42:59 +0530

    Question G Nom Still loves Peanut Butter Solved Lb3

---
+++ labsheet-3/QuestionG/QuestionG.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+// why does left love peanut butter yaar
+int main()
+{
+    // taking input
+    int length;
+    scanf("%d", &length);
+    int arr[length];
+    for (int i = 0; i < length; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    // intiialisation
+    int maximum_weight = 0;
+    int left_pointer = 0;
+    int right_pointer = length - 1;
+    int left_sum = 0;
+    int right_sum = 0;
+    // approach set 2 pointers one from left andd one from right
+    // and add weights to left sum and right sum
+    // when they are equal set them to max weight
+    // until they cross each other
+    while (left_pointer < right_pointer)
+    {
+        // adding to right sum and left sum
+        left_sum += arr[left_pointer];
+        right_sum += arr[right_pointer];
+        if (left_sum > right_sum)
+        {
+            right_pointer--;
+        }
+        if (right_sum > left_sum)
+        {
+            left_pointer++;
+        }
+        if (left_sum == right_sum)
+        {
+            maximum_weight = right_sum;
+            left_pointer++;
+            right_pointer--;
+        }
+    }
+    printf("%d", maximum_weight);
+    return 0;
+}
