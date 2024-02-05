cdbced60a6088f3b1e699946e09d09619de9d289
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-06 03:46:36 +0530

    fixed issue with b

--- labsheet-3_Sliding_Window_and_2_pointers/QuestionB/QuestionB.c
+++ labsheet-3_Sliding_Window_and_2_pointers/QuestionB/QuestionB.c
@@ -3,9 +3,9 @@
 int main()
 {
     int len;
-    // creating a map to count occurences
+    // creating a map to count occurrences
     int map[26] = {0};
-    // taking input & initialisation
+    // taking input & initialization
     scanf("%d", &len);
     char word[len];
     int current = 0;
@@ -13,32 +13,28 @@ int main()
     int curr_l = 0;
     int longest = 0;
     scanf("%s", word);
-    //
+
     for (int i = 0; i < len; i++)
     {
-        if (!(map[(int)word[i] - 97]))
+        if (map[(int)word[i] - 'a'] == 0 || map[(int)word[i] - 'a'] < current)
         {
-            map[(int)word[i] - 97] = i;
+            map[(int)word[i] - 'a'] = i + 1; // Update map to store position
             endofcurrent = i;
-            curr_l = endofcurrent - current + 1;
         }
         else
         {
-            current = map[(int)word[i] - 97] + 1;
-            if (curr_l > longest)
-            {
-                longest = curr_l;
-            }
-            // Reset the map for the next substring
-            for (int j = 0; j < 26; j++)
-            {
-                map[j] = 0;
-            }
+            current = map[(int)word[i] - 'a'];
+            map[(int)word[i] - 'a'] = i + 1; // Update map to store position
+            endofcurrent = i;
+        }
+
+        curr_l = endofcurrent - current + 1;
+        if (curr_l > longest)
+        {
+            longest = curr_l;
         }
     }
-    if (curr_l > longest)
-    {
-        longest = curr_l;
-    }
+
     printf("%d", longest);
+    return 0;
 }
