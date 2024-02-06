94272f52c813c15aab7fe35786601534573341b8
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-06 06:43:21 +0530

    solved c 2 times

---
+++ labsheet-3_Sliding_Window_and_2_pointers/QuestionC/practice.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char sentence[n];
+    scanf(" %[^\n]s", sentence);
+    //
+    int pointer = n - 1;
+    int result_pointer = 0;
+    char reversed_words[n];
+    char result[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (sentence[i] != ' ')
+        {
+            reversed_words[pointer--] = sentence[i];
+        }
+        else
+        {
+            reversed_words[pointer] = '\0';
+            while (pointer < n - 1)
+            {
+                result[result_pointer++] = reversed_words[++pointer];
+            }
+            if (i < n - 1)
+            {
+                result[result_pointer++] = ' ';
+            }
+            pointer = n - 1;
+        }
+    }
+    while (pointer < n - 1)
+    {
+        result[result_pointer++] = reversed_words[++pointer];
+    }
+    result[result_pointer] = '\0';
+    printf("%s", result);
+    return 0;
+}
