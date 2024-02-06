89697a3a86a84cdd02d6699c6966d04520e566f8
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-06 05:35:48 +0530

    solved b once

--- labsheet-3_Sliding_Window_and_2_pointers/QuestionB/practice.c
+++ labsheet-3_Sliding_Window_and_2_pointers/QuestionB/practice.c
@@ -2,4 +2,36 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    char word[n];
+    int map[26] = {0};
+    int current = 0;
+    int endofcurrent = 0;
+    int curr_l = 0;
+    int longest = 0;
+    scanf("%s", word);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (map[(int)word[i] - 'a'] == 0 || map[(int)word[i] - 'a'] < current)
+        {
+            map[(int)word[i] - 'a'] = i + 1;
+            endofcurrent = i;
+        }
+        else
+        {
+            current = map[(int)word[i] - 'a'];
+            map[(int)word[i] - 'a'] = i + 1;
+            endofcurrent = i;
+        }
+        curr_l = endofcurrent - current + 1;
+        if (curr_l > longest)
+        {
+            longest = curr_l;
+        }
+    }
+    printf("%d\n", longest);
+    return 0;
 }
