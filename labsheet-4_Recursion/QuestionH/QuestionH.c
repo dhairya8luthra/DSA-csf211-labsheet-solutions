4f1da0e4817697747b72fee5b849ad779a158f00
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-12 16:57:20 +0530

    QuestionH

---
+++ labsheet-4_Recursion/QuestionH/QuestionH.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // taking input
+    int n, kira_wins, nom_wins;
+    scanf("%d", &n);
+    scanf("%d", &kira_wins);
+    scanf("%d", &nom_wins);
+    //
+    // approach no need to simulate strings we take 1 for kira win and 0 for nom win
+    // backtrack for all combinations
+    // keep in account the minimums
+}
