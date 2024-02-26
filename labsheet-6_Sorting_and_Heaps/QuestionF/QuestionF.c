528f3c0075e34552e514039bff248b3225e210bb
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-26 07:39:34 +0530

    QuestionF solved

---
+++ labsheet-6_Sorting_and_Heaps/QuestionF/QuestionF.c
@@ -0,0 +1,96 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+int merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int inv_count = 0;
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+            inv_count += (n1 - i);
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+    return inv_count;
+}
+
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+int mergeSort(int arr[], int l, int r)
+{
+    int inv_count = 0;
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        inv_count += mergeSort(arr, l, m);
+        inv_count += mergeSort(arr, m + 1, r);
+
+        inv_count += merge(arr, l, m, r);
+    }
+    return inv_count;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int inv_count = mergeSort(arr, 0, n - 1);
+
+    printf("%d\n", inv_count);
+    return 0;
+}
