3111f0d6b33a3976487da3957c032e7f4a21eab4
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-02-26 19:56:34 +0530

    Question C practiced 4 times

--- labsheet-6_Sorting_and_Heaps/QuestionC/practice.c
+++ labsheet-6_Sorting_and_Heaps/QuestionC/practice.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 void swap(int *a, int *b)
 {
     int temp = *a;
@@ -9,23 +10,22 @@ void swap(int *a, int *b)
 void Max_heapify(int arr[], int n, int i)
 {
     int largest = i;
-    int left = 2 * i + 1;
     int right = 2 * i + 2;
-    if (left < n && arr[largest] < arr[left])
+    int left = 2 * i + 1;
+    if (left < n && arr[left] > arr[largest])
     {
         largest = left;
     }
-    if (right < n && arr[largest] < arr[right])
+    if (right < n && arr[right] > arr[largest])
     {
         largest = right;
     }
     if (largest != i)
     {
-        swap(&arr[largest], &arr[i]);
+        swap(&arr[i], &arr[largest]);
         Max_heapify(arr, n, largest);
     }
 }
-
 void heapSort(int arr[], int n)
 {
     for (int i = n / 2; i >= 0; i--)
@@ -34,7 +34,7 @@ void heapSort(int arr[], int n)
     }
     for (int i = n - 1; i >= 0; i--)
     {
-        swap(&arr[i], &arr[0]);
+        swap(&arr[0], &arr[i]);
         Max_heapify(arr, i, 0);
     }
 }
