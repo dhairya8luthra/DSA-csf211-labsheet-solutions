feabf4b4885a097c685f05639f995d0c3e2313f8
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-03-26 03:22:24 +0530

    practiced Question A twice

---
+++ labsheet-8_Stacks_and_Queues/QuestionA/practice/practice2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 1000000
+
+void push(char stack[], int *top, char c)
+{
+    (*top)++;
+    stack[(*top)] = c;
+}
+
+char pop(char stack[], int *top)
+{
+    char c = stack[(*top)];
+    (*top)--;
+    return c;
+}
+
+int main()
+{
+    char stack[MAX_SIZE];
+    int top = -1;
+    int isNo = 0;
+    char st[MAX_SIZE];
+    scanf("%s", st);
+    int n = strlen(st);
+    for (int i = 0; i < n; i++)
+    {
+        if (st[i] == '(' || st[i] == '{' || st[i] == '[')
+        {
+            push(stack, &top, st[i]);
+        }
+        else
+        {
+            if (top == -1)
+            {
+                isNo = 1;
+                printf("NO");
+                break;
+            }
+            char c = pop(stack, &top);
+            if ((st[i] == '}' && c != '{') || (st[i] == ']' && c != '[') || (st[i] == ')' && c != '('))
+            {
+                isNo = 1;
+                printf("NO");
+                break;
+            }
+        }
+    }
+    if (top != -1 && !isNo)
+    {
+        printf("NO");
+        isNo = 1;
+    }
+
+    if (!isNo)
+    {
+        printf("YES");
+    }
+
+    return 0;
+}
